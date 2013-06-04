5e73ae8419600c2f3cdc23261d068950e14bbced
Darren Cauthon <darren@cauthon.com>
2013-06-04 06:37:42 -0500

    Add the need for data points.

--- lib/panic_board_data/graph.rb
+++ lib/panic_board_data/graph.rb
@@ -1,9 +1,34 @@
 module PanicBoardData
-  class Graph
-    attr_accessor :title
 
-    def to_hash
-      { 'graph' => { 'title' => 'Soft Drink Sales' } }
+  class DataSequence
+    attr_accessor :data
+    def initialize(name)
+      @data = {}
     end
   end
+
+  class Graph
+    attr_accessor :title
+    attr_accessor :data_sequences
+
+    def initialize
+      @data_sequences = []
+    end
+
+    def to_hash
+      { 
+        'graph' => { 
+                     'title' => 'Soft Drink Sales',
+                     'datapoints' => [ 
+                                       { 'title' => '2008', 'value' => 22 },
+                                       { 'title' => '2009', 'value' => 24 },
+                                       { 'title' => '2010', 'value' => 25.5 },
+                                       { 'title' => '2011', 'value' => 27.9 },
+                                       { 'title' => '2012', 'value' => 31 }
+                                     ]
+                   }
+      }
+    end
+  end
+
 end
--- spec/panic_board_data/graph_spec.rb
+++ spec/panic_board_data/graph_spec.rb
@@ -5,17 +5,38 @@ describe PanicBoardData::Graph do
     PanicBoardData::Graph.nil?.must_equal false
   end
 
+  it "should default to an empty data_sequence array" do
+    PanicBoardData::Graph.new.data_sequences.count.must_equal 0
+  end
+
   describe "to_hash" do
 
     let(:graph) { PanicBoardData::Graph.new }
 
     before do
       graph.title = "Soft Drink Sales"
+
+      data_sequence = PanicBoardData::DataSequence.new('X-Cola')
+      data_sequence.data['2008'] = 22
+      data_sequence.data['2009'] = 24
+      data_sequence.data['2010'] = 25.5
+      data_sequence.data['2011'] = 27.9
+      data_sequence.data['2012'] = 31
+
+      graph.data_sequences << data_sequence
       @result = graph.to_hash
     end
 
     it "should set the title" do
       @result['graph']['title'] = 'Soft Drink Sales'
+    end
+
+    it "should add the data sequences" do
+      @result['graph']['datapoints'][0].must_equal( { 'title' => '2008', 'value' => 22 } )
+      @result['graph']['datapoints'][1].must_equal( { 'title' => '2009', 'value' => 24 } )
+      @result['graph']['datapoints'][2].must_equal( { 'title' => '2010', 'value' => 25.5 } )
+      @result['graph']['datapoints'][3].must_equal( { 'title' => '2011', 'value' => 27.9 } )
+      @result['graph']['datapoints'][4].must_equal( { 'title' => '2012', 'value' => 31 } )
     end
 
 let(:expected_result) do 
