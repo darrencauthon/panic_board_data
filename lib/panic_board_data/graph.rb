fcf6d5ca3af7f410279baa7d5b4ec4179542162d
Darren Cauthon <darren@cauthon.com>
2013-06-04 09:01:46 -0500

    Refactor.

--- lib/panic_board_data/graph.rb
+++ lib/panic_board_data/graph.rb
@@ -19,20 +19,28 @@ module PanicBoardData
                      'color'         => formatted_color,
                      'type'          => type.to_s,
                      'total'         => total.to_s,
-                     'datasequences' => data_sequences.map do |data_sequence|
-                                         {
-                                           'title' => data_sequence.title,
-                                           'refreshEveryNSeconds' => 120,
-                                           'datapoints' => data_sequence.data.map do |k, v|
-                                                            { 'title' => k, 'value' => v }
-                                                          end
-                                         }
-                                        end
+                     'datasequences' => formatted_data_sequences
                    }
       }
     end
 
     private
+
+    def formatted_data_sequences
+      data_sequences.map do |data_sequence|
+         {
+           'title'                => data_sequence.title,
+           'refreshEveryNSeconds' => 120,
+           'datapoints'           => data_points_for(data_sequence)
+         }
+      end
+    end
+
+    def data_points_for data_sequence
+      data_sequence.data.map do |k, v|
+        { 'title' => k, 'value' => v }
+      end
+    end
 
     def formatted_color
       color == :light_gray ? 'lightGray' : color.to_s
