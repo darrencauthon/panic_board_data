35af28ebadbfe2a451741abcde31d091622ac29a
Darren Cauthon <darren@cauthon.com>
2013-11-06 17:03:43 -0600

    Implement a to_csv.

--- lib/panic_board_data/table.rb
+++ lib/panic_board_data/table.rb
@@ -1,4 +1,12 @@
+require 'csv'
+
 module PanicBoardData
   class Table
+
+    def self.to_csv data
+      ::CSV.generate do |csv|
+        data.each { |row| csv << row }
+      end.strip
+    end
   end
 end
--- spec/panic_board_data/table_spec.rb
+++ spec/panic_board_data/table_spec.rb
@@ -4,4 +4,46 @@ describe PanicBoardData::Table do
   it "should exist" do
     PanicBoardData::Table.nil?.must_equal false
   end
+
+  describe "to_csv" do
+
+    before do
+      @result = PanicBoardData::Table.to_csv data
+    end
+
+    describe "an empty set" do
+      let(:data) { [] }
+
+      it "should return an empty string" do
+        @result.must_equal ''
+      end
+    end
+
+    [:array, :result].to_objects { [
+      [ [0],        "0"            ],
+      [ [1],        "1"            ],
+      [ [1,2],      "1,2"          ],
+      [ [3,4, '"'], "3,4,\"\"\"\"" ]
+    ] }.each do |test|
+      describe "one row" do
+        let(:data) { [ test.array ] }
+
+        it "should return the single value" do
+          @result.must_equal test.result
+        end
+      end
+    end
+
+    [:first_row, :second_row, :result].to_objects { [
+      [ [0], [1], "0\n1" ],
+    ] }.each do |test|
+      describe "two rows" do
+        let(:data) { [ test.first_row, test.second_row ] }
+
+        it "should return the single value" do
+          @result.must_equal test.result
+        end
+      end
+    end
+  end
 end
