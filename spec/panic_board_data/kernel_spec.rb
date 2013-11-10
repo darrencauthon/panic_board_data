174717132cf43d4545d7bfd4ee2ae54306c9bc1b
Darren Cauthon <darren@cauthon.com>
2013-11-10 14:41:14 -0600

    Test refactor.

--- spec/panic_board_data/kernel_spec.rb
+++ spec/panic_board_data/kernel_spec.rb
@@ -1,21 +1,18 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe "progress_bar_to" do
-  [:value, :result].to_objects {[
-    [1],
-    [2],
-    [8]
-  ]}.each do |test|
+
+  [1, 2, 3, 4, 5, 6, 7, 8].each do |value|
 
     describe "progress bars" do
 
       before do
-        @result = progress_bar_to(test.value)
+        @result = progress_bar_to(value)
       end
 
       it "should create a progress bar with the right value" do
         @result.is_a?(PanicBoardData::ProgressBar).must_equal true
-        @result.value.must_equal test.value
+        @result.value.must_equal value
       end
 
     end
