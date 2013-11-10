9fba036384e09740837093a081696f69a3efdcd5
Darren Cauthon <darren@cauthon.com>
2013-11-10 14:54:11 -0600

    Simplify.

--- lib/panic_board_data/kernel.rb
+++ lib/panic_board_data/kernel.rb
@@ -7,13 +7,8 @@ module Kernel
 
   def build_image value
     url_for = ->(value) do
-                          [value]
-                            .select { |x| x.to_s != '' }
-                            .map    { |x| x.to_s.strip }
-                            .map    { |x| x.gsub('/', '') }
-                            .join('/')
-                            .gsub('http:', 'http://')
-                            .gsub('https:', 'https://')
+                          value.to_s.gsub('http:', 'http://')
+                                    .gsub('https:', 'https://')
                         end
 
     "<img src=\"#{url_for.call(value)}\" />"
